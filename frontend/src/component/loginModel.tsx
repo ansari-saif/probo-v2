a488bb5fd84ac3b2be80e9a5554d2db060186cb5
Saif Husain Ansari <saifhussainansari6@gmail.com>
2024-11-09 12:42:32 +0530

    working on login flow

--- frontend/src/component/loginModel.tsx
+++ frontend/src/component/loginModel.tsx
@@ -4,17 +4,86 @@ import { useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 
 function loginModel({ isOpen, setIsOpen }) {
-
+    const [firstStep, setFirstStep] = useState(true)
     const [phoneNumber, setPhoneNumber] = useState('');
+
+    const [otp1, setOtp1] = useState("");
+    const [otp2, setOtp2] = useState("");
+    const [otp3, setOtp3] = useState("");
+    const [otp4, setOtp4] = useState("");
+    const [otp5, setOtp5] = useState("");
+    const [otp6, setOtp6] = useState("");
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPhoneNumber(e.target.value);
     };
+    const handleOtpChange = (e: React.ChangeEvent<HTMLInputElement>, func) => {
+        func(e.target.value);
+    };
 
     const handleSubmit = () => {
-        // Handle OTP request submission here
-        console.log('Requesting OTP for:', phoneNumber);
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "mobile": phoneNumber
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("http://localhost:8000/api/v1/user/login", requestOptions)
+            .then((response) => {
+                if (response.status === 200) {
+                    setFirstStep(false);
+                    console.log("success");
+                } else {
+                    console.log("failed");
+                }
+                return response.json()
+            }
+            )
+            .then((result) => {
+                // success logic here
+            })
+            .catch((error) => console.error(error));
     };
+
+    const handleSubmitVerifyOtp = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const otp = otp1 + otp2 + otp3 + otp4 + otp5 + otp6;
+        const raw = JSON.stringify({
+            "mobile": phoneNumber,
+            "otp": otp
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("http://localhost:8000/api/v1/user/validateOtp", requestOptions)
+            .then((response) => {
+                if (response.status === 200) {
+                    console.log("success");
+                } else {
+                    console.log("failed");
+                }
+                return response.text()
+            })
+            .then((result) => {
+                // logic here
+            })
+            .catch((error) => console.error("error - code phat gya "));
+    };
+    
     return (
         <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="fixed inset-0 z-10 flex items-center justify-center">
             <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
@@ -24,33 +93,141 @@ function loginModel({ isOpen, setIsOpen }) {
                             <AiOutlineClose className="h-6 w-6" />
                         </button>
                     </div>
-                    <h2 className="text-2xl font-semibold text-gray-800 mb-4">Enter your mobile number</h2>
-                    <p className="text-gray-600 mb-4">We’ll send you an OTP</p>
-                    <div className="flex items-center mb-4 border rounded-md overflow-hidden">
-                        <span className="bg-gray-200 px-3 py-2 text-gray-800">+91</span>
-                        <input
-                            type="text"
-                            placeholder="Phone number"
-                            value={phoneNumber}
-                            onChange={handleInputChange}
-                            className="w-full p-2 focus:outline-none"
-                        />
-                    </div>
-                    <p className="text-xs text-gray-500 mb-4">
-                        By continuing, you accept that you are 18+ years of age & agree to the{' '}
-                        <a href="#" className="text-blue-500 hover:underline">
-                            Terms and Conditions
-                        </a>
-                        .
-                    </p>
-                    <button
-                        onClick={handleSubmit}
-                        disabled={!phoneNumber}
-                        className={`w-full py-2 rounded-md text-white ${phoneNumber ? 'bg-blue-500 hover:bg-blue-600' : 'bg-gray-300'
-                            }`}
-                    >
-                        Get OTP
-                    </button>
+                    {
+                        firstStep ?
+                            <>
+                                <h2 className="text-2xl font-semibold text-gray-800 mb-4">Enter your mobile number</h2>
+                                <p className="text-gray-600 mb-4">We’ll send you an OTP</p>
+                                <div className="flex items-center mb-4 border rounded-md overflow-hidden">
+                                    <span className="bg-gray-200 px-3 py-2 text-gray-800">+91</span>
+                                    <input
+                                        type="text"
+                                        placeholder="Phone number"
+                                        value={phoneNumber}
+                                        onChange={handleInputChange}
+                                        className="w-full p-2 focus:outline-none"
+                                    />
+                                </div>
+                                <p className="text-xs text-gray-500 mb-4">
+                                    By continuing, you accept that you are 18+ years of age & agree to the{' '}
+                                    <a href="#" className="text-blue-500 hover:underline">
+                                        Terms and Conditions
+                                    </a>
+                                    .
+                                </p>
+                                <button
+                                    onClick={handleSubmit}
+                                    disabled={!phoneNumber}
+                                    className={`w-full py-2 rounded-md text-white ${phoneNumber ? 'bg-blue-500 hover:bg-blue-600' : 'bg-gray-300'}`}
+                                >
+                                    Get OTP
+                                </button>
+                            </> :
+                            <>
+                                <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+                                    <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-6">
+                                        <h2 className="text-xl font-semibold text-gray-800 mb-2">Verify phone</h2>
+                                        <p className="text-gray-600 mb-4">OTP has been sent to +91-{phoneNumber}</p>
+
+                                        <div className="flex justify-center gap-2 mb-4">
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp1} 
+                                                onChange={(e) => handleOtpChange(e, setOtp1)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key !== "Backspace" && e.target.value) {
+                                                        document.querySelector('input[name="otp2"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp1"
+                                            />
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp2}
+                                                onChange={(e) => handleOtpChange(e, setOtp2)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key === "Backspace" && !e.target.value) {
+                                                        document.querySelector('input[name="otp1"]')?.focus();
+                                                    } else if(e.key !== "Backspace" && e.target.value) {
+                                                        document.querySelector('input[name="otp3"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp2"
+                                            />
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp3}
+                                                onChange={(e) => handleOtpChange(e, setOtp3)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key === "Backspace" && !e.target.value) {
+                                                        document.querySelector('input[name="otp2"]')?.focus();
+                                                    } else if(e.key !== "Backspace" && e.target.value) {
+                                                        document.querySelector('input[name="otp4"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp3"
+                                            />
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp4}
+                                                onChange={(e) => handleOtpChange(e, setOtp4)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key === "Backspace" && !e.target.value) {
+                                                        document.querySelector('input[name="otp3"]')?.focus();
+                                                    } else if(e.key !== "Backspace" && e.target.value) {
+                                                        document.querySelector('input[name="otp5"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp4"
+                                            />
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp5}
+                                                onChange={(e) => handleOtpChange(e, setOtp5)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key === "Backspace" && !e.target.value) {
+                                                        document.querySelector('input[name="otp4"]')?.focus();
+                                                    } else if(e.key !== "Backspace" && e.target.value) {
+                                                        document.querySelector('input[name="otp6"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp5"
+                                            />
+                                            <input 
+                                                type="text" 
+                                                maxLength={1} 
+                                                value={otp6}
+                                                onChange={(e) => handleOtpChange(e, setOtp6)}
+                                                onKeyUp={(e) => {
+                                                    if(e.key === "Backspace" && !e.target.value) {
+                                                        document.querySelector('input[name="otp5"]')?.focus();
+                                                    }
+                                                }}
+                                                className="w-10 h-10 border border-gray-300 rounded-md text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                                name="otp6"
+                                            />
+                                        </div>
+                                        <button
+                                            onClick={handleSubmitVerifyOtp}
+                                            className={`w-full py-2 rounded-md text-white bg-blue-500 hover:bg-blue-600`}
+                                        >
+                                            Verify
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </>
+                    }
                 </div>
             </div>
         </Dialog>
--- todo.md
+++ todo.md
@@ -1,7 +1,26 @@
 user onoard init 15 rupees
-add event 
-    admin create event page
-    integrated api add event api
-
+create event on postman
 integrate trade api
 integrate websocket
+
+login and store data in the localstorage .
+    if token not found in the local or any api failed then reverse to the login popup and change header content 
+    else should be working fine
+all apis will get token from localstorage itself
+
+
+phelel frontend ka karo 
+    login api hit on success oto inbox should be visible on submit otp will store otp in local and show success or failure toast 
+    taost integration
+
+
+otp toast 
+    taost posision 
+        middle top
+
+    text while sending otp 
+        """Please check SMS section for the OTP!!"""
+
+    on failure 
+        """Logged in successfully"""
+    on success
