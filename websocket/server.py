63aed0f9ec4d0186aa542a83ebd2f0e3b98f4e68
Saif Husain Ansari <saifhussainansari6@gmail.com>
2024-11-10 11:13:00 +0530

    changes

--- websocket/server.py
+++ websocket/server.py
@@ -58,4 +58,4 @@ async def websocket_endpoint(websocket: WebSocket, room_id: str):
         await manager.broadcast(f"Client left the room", room_id)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
